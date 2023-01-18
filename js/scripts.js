a634efdcc9909f57018ded26f70c888ff7bfa25d
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-01-18 15:42:56 +0100

    Update scripts.js
    if applied, this commit will add objects to the array with the 3 similar keys and their values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,1 @@
-let pokemonList[]
+let pokemonList = [{ name: 'pikachu', height: '0.4', type: 'electric' }, { name: 'ivysaur', height: '1', type: ['grass', 'poison'] }, { name: 'squirtle', height: '0.5', type: 'water' }]
