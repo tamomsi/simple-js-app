ea934adccafcc91039020959a5e42a8761c042cf
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-01-18 16:01:37 +0100

    Update scripts.js
    if applied, this commit will arrange the code better and add a new  object to the array with its keys and values

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-let pokemonList = [{ name: 'pikachu', height: '0.4', type: 'electric' }, { name: 'ivysaur', height: '1', type: ['grass', 'poison'] }, { name: 'squirtle', height: '0.5', type: 'water' }]
+let pokemonList = [
+    { name: 'pikachu', height: 0.4, type: 'electric' },
+    { name: 'ivysaur', height: 1, type: ['grass', 'poison'] },
+    { name: 'squirtle', height: 0.5, type: 'water' },
+    { name: 'charmander', height: 0.6, type: 'fire' }
+];
