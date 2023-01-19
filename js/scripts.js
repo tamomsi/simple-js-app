ef668bdc2bbd1754757e3f00a2b145347cc2cc18
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-01-19 11:07:19 +0100

    Update scripts.js
    If applied, this commit will change the key named type into types.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-    { name: 'pikachu', height: 0.4, type: 'electric' },
-    { name: 'ivysaur', height: 1, type: ['grass', 'poison'] },
-    { name: 'squirtle', height: 0.5, type: 'water' },
-    { name: 'charmander', height: 0.6, type: 'fire' }
+    { name: 'pikachu', height: 0.4, types: 'electric' },
+    { name: 'ivysaur', height: 1, types: ['grass', 'poison'] },
+    { name: 'squirtle', height: 0.5, types: 'water' },
+    { name: 'charmander', height: 0.6, types: 'fire' }
 ];
