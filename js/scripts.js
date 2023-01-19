0dc54374cbbde5d6eec2fae5755a4d4f3cdc363b
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-01-19 18:29:38 +0100

    Update scripts.js
    if applied, this commit will add a loop of pokemonList and write the names of pokemons in capital letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 let pokemonList = [
-    { name: 'pikachu', height: 0.4, types: 'electric' },
-    { name: 'ivysaur', height: 1, types: ['grass', 'poison'] },
-    { name: 'squirtle', height: 0.5, types: 'water' },
-    { name: 'charmander', height: 0.6, types: 'fire' }
+    { name: 'Pikachu', height: 0.4, types: 'electric' },
+    { name: 'Ivysaur', height: 1, types: ['grass', 'poison'] },
+    { name: 'Squirtle', height: 0.5, types: 'water' },
+    { name: 'Charmander', height: 0.6, types: 'fire' }
 ];
+
+//this is a loop for pokemonList
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height < 1) {
+        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ')<br>');
+    } else if (pokemonList[i].height >= 1) {
+        document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ') -Wow, that\'s big!<br>');
+    }
+}
