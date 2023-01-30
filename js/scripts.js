d52e587e8956a9ff23cf99f2a1ec69b8cb2f2792
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-01-30 18:26:24 +0100

    Update scripts.js
    if applied, this commit will add an API url of pokemons to pokemon repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
     function getAll() {
         return pokemonList;
