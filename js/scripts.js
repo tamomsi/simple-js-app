b48b8c4dbb7d023075f5bcc9f5ca714d50c00e27
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-02-09 12:26:16 +0100

    Update scripts.js
    if applied, this commit will add item types code in loaddetails

--- js/scripts.js
+++ js/scripts.js
@@ -44,7 +44,7 @@ let pokemonRepository = (function () {
         }).then(function (details) {
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
-            item.types = details.types;
+            item.types = details.types.map((type) => type.type.name);
         }).catch(function (e) {
             console.error(e);
         });
