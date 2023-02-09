cf577a25f6f51f84de28dc608670dec57c77aba0
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-02-09 12:04:03 +0100

    Update scripts.js
    if applied, this commit will update the addlistitem function with jquery code

--- js/scripts.js
+++ js/scripts.js
@@ -10,17 +10,16 @@ let pokemonRepository = (function () {
     }
 
     function addListItem(pokemon) {
-        let pokemonList = document.querySelector('.pokemon-list');
-        let listItem = document.createElement('li');
-        let button = document.createElement('button');
-        button.innerText = pokemon.name;
-        button.classList.add('button-class')
-        listItem.appendChild(button);
-        pokemonList.appendChild(listItem);
-        button.addEventListener('click', function (event) {
+        let pokemonListElement = $();
+        let listItem = $('.pokemon-list');
+        let button = $('<button class="button-class" data-target="#pokemon-modal" data-toggle="modal">' + pokemon.name + '</button>');
+
+        listItem.append(button);
+        pokemonListElement.append(listItem);
+        button.on('click', function () {
             showDetails(pokemon);
-        })
-    };
+        });
+    }
 
     function loadList(pokemon) {
         return fetch(apiUrl).then(function (response) {
